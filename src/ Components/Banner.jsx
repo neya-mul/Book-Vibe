bc77f266c0d5e9a15b0d3390ef143480c2dab00a
neya-mul <neyamulislam946@gmail.com>
2026-04-06 17:26:20 +0600

    banner added

---
+++ src/ Components/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import banner from "../assets/pngwing 1.png"
+
+export default function Banner() {
+    return (
+        <div className="hero bg-base-200 min-h-[70vh] rounded-2xl">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img
+                    src={banner}
+                    className="max-w-sm rounded-lg"
+                />
+                <div className='space-y-7'>
+                    <h1 className="text-5xl font-bold">Books to freshen up <br /> your bookshelf</h1>
+
+                    <button className="btn btn-success text-white">View The List</button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Banner from '../ Components/Banner'
 
 export default function Homepage() {
   return (
-    <div>Homepage</div>
+    <div className=' container mx-auto my-2'>
+      <Banner></Banner>
+
+    </div>
   )
 }
