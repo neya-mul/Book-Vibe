017d58ac71af4606d797a8839caa9bbf4b23ea85
neya-mul <neyamulislam946@gmail.com>
2026-04-07 19:43:57 +0600

    react tabs added

--- src/Pages/Books.jsx
+++ src/Pages/Books.jsx
@@ -1,12 +1,35 @@
 import React, { useContext } from 'react'
 import { bookContext } from '../context/BookProvider';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 export default function Books() {
-  const { storedBook } = useContext(bookContext)
+  const { storedBook, wishList } = useContext(bookContext)
   // console.log(BookContext)
-  console.log(   storedBook);
+  console.log(storedBook);
+  console.log(wishList)
 
   return (
-    <div>Books</div>
+    <div className='container mx-auto my-6'>
+
+      <div>
+        <Tabs>
+          <TabList>
+            <Tab> <h1>Read List : {storedBook.length}</h1></Tab>
+            <Tab> <h1>Wish List : {wishList.length}</h1></Tab>
+          </TabList>
+
+          <TabPanel>
+            <h2>read list</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>wishList</h2>
+          </TabPanel>
+        </Tabs>
+      </div>
+
+
+
+    </div>
   )
 }
