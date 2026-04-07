98b16fe701e71e161f33df57255a1ead2ebd2786
neya-mul <neyamulislam946@gmail.com>
2026-04-07 14:28:32 +0600

    book details added

--- src/Pages/BookDetails.jsx
+++ src/Pages/BookDetails.jsx
@@ -12,12 +12,34 @@ export default function BookDetails() {
 
 
     return (
-        <div className='container mx-auto my-10 flex justify-between items-center'>
-            <div className=''>
-                <img src={expectedBook.image} alt="" />
+        // main div
+        <div className='container mx-auto justify-between items-center flex gap-5 flex-col lg:flex-row mt-10 text-center lg:text-left'>
+            {/* image div */}
+            <div className='max-w-[500px]  mx-auto p-10'>
+                <img src={expectedBook.image} alt="" className='h-[700px] mx-auto' />
             </div>
-            <div>
-                <h1>{expectedBook.bookName}</h1>
+            {/* description */}
+            <div className=' max-w-[600px] space-y-10 p-6 mx-auto'>
+                <div className='border-b border-gray-400'>
+                    <h1 className='text-5xl'>{expectedBook.bookName}</h1>
+                    <p>By:{expectedBook.author}</p>
+                </div>
+                <div className='border-b border-gray-400 py-6'>
+                    {
+                        expectedBook.tags.map((tag) => (
+                            <span className='border border-green-400 text-green-500 rounded-2xl p-1'>{tag}</span>
+                        ))
+                    }
+                </div>
+                <div className='border-b border-gray-400 py-5'>
+                    <p><span className='font-bold '>Review: </span>{expectedBook.review}</p>
+                </div>
+                <div>
+                    <p>Number or pages : {expectedBook.totalPages}</p>
+                    <p>Publisher : {expectedBook.publisher}</p>
+                    <p>Year of Publishing : {expectedBook.yearOfPublishing}</p>
+                    <p>Rating : {expectedBook.rating}</p>
+                </div>
             </div>
         </div>
     )
